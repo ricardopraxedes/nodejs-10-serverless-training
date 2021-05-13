9900367c6f051f1af420df5a98416e7caf9399e0
ricardopraxedes <ricardopraxedes1@gmail.com>
2021-05-13 16:09:45 -0300

    add function to generate certificate pdf

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -44,6 +44,10 @@ export async function handle(event: { body: string }) {
         }
     }).promise()
 
+    const templateHtml = handleTemplate({ id, name, grade })
+
+    await generatePdf(templateHtml)
+
     return {
         statusCode: 201,
         body: JSON.stringify({
@@ -53,4 +57,49 @@ export async function handle(event: { body: string }) {
             "Content-Type": "application/json"
         }
     }
+}
+
+function handleTemplate({ id, name, grade }: IRequest) {
+
+    const date = dayjs().toString()
+
+    const medalPath = path.join(process.cwd(), "src", "template", "selo.png")
+
+    const medal = fs.readFileSync(medalPath, "base64")
+
+    const templatePath = path.join(process.cwd(), "src", "template", "certificate.hbs")
+
+    const templateContent = fs.readFileSync(templatePath, "utf-8")
+
+    const template = handlebars.compile(templateContent)
+
+    const templateHtml = template({ date, medal, id, name, grade })
+
+    return templateHtml
+}
+
+async function generatePdf(templateHtml: string) {
+
+    const browser = await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: true,
+    })
+
+    const page = await browser.newPage()
+
+    await page.setContent(templateHtml)
+
+    const pdf = await page.pdf({
+        format: "a4",
+        landscape: true,
+        printBackground: true,
+        preferCSSPageSize: true,
+        path: process.env.IS_OFFLINE ? "certificate.pdf" : null
+    })
+
+    await browser.close()
+
+    return pdf
 }
